ceeb2534eb97c91d6d529d3eaae6118c5f225c5a
semih1239 <semihayyildiz23@hotmail.com>
2022-03-06 01:19:10 +0100

    Week2 Homework is Added

---
+++ week2/task1/Program.cs
@@ -0,0 +1,46 @@
+﻿var time = new JupiterTime();
+time.Hours = 9;
+time.Minutes = 128;
+
+PrintTime(time);
+
+void PrintTime(JupiterTime time)
+{
+    if (time.Minutes < 10) Console.WriteLine($"{time.Hours}:0{time.Minutes}");
+    else Console.WriteLine($"{time.Hours}:{time.Minutes}");
+}
+class JupiterTime
+{
+    private int _hours = 0, _minutes = 0;
+    public int Hours
+    {
+        get
+        {
+            return _hours;
+        }
+        set
+        {
+            if (value < 0) throw new Exception("WHAT THE HACK!!!");
+            else if (value > 9) _hours = (value % 10);
+            else _hours = value;
+        }
+    }
+    public int Minutes
+    {
+        get
+        {
+            return _minutes;
+        }
+        set
+        {
+            if (value < 0) throw new Exception("WHAT THE HACK!!!");
+            else if (value > 60)
+            {
+                _hours = _hours + (value / 60);
+                if (_hours > 9) _hours = _hours - 10;
+                _minutes = (value % 60);
+            }
+            else _minutes = value;
+        }
+    }
+}
---
+++ week2/task7/Program.cs
@@ -0,0 +1,103 @@
+﻿var signaler = new Signaler();
+signaler.AddTime(new JupiterTime(2, 00));
+signaler.AddTime(new JupiterTime(4, 00));
+signaler.AddTime(new JupiterTime(6, 00));
+signaler.Check(new JupiterTime(4, 21));
+
+class JupiterTime
+{
+    public int _hours = 0, _minutes = 0;
+
+    public override string ToString()
+    {
+        if (_minutes < 10) return _hours + ":0" + _minutes;
+        return _hours + ":" + _minutes;
+    }
+    public int Hours
+    {
+        get
+        {
+            return _hours;
+        }
+        set
+        {
+            if (value < 0) _hours = (10 + (value % 10));
+            else if (value > 9) _hours = (value % 10);
+            else _hours = value;
+        }
+    }
+    public int Minutes
+    {
+        get
+        {
+            return _minutes;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                _hours = _hours + (value / 60);
+                if (_hours < 0) _hours += 10;
+                _minutes = _minutes + (value % 60);
+                if (_minutes < 0)
+                {
+                    _minutes += 60;
+                    _hours -= 1;
+                }
+            }
+            else if (value > 59)
+            {
+                _hours = _hours + (value / 60);
+                if (_hours > 9) _hours -= 10;
+                _minutes = (value % 60);
+            }
+            else _minutes = value;
+        }
+    }
+
+    public JupiterTime(int hours, int minutes)
+    {
+        Hours = hours;
+        Minutes = minutes;
+    }
+    public JupiterTime AddHours(int number)
+    {
+        Hours = _hours + number;
+        return new JupiterTime(_hours, _minutes);
+    }
+    public JupiterTime AddMinutes(int number)
+    {
+        Minutes = number;
+        return new JupiterTime(_hours, _minutes);
+    }
+}
+
+class Signaler
+{
+    List<JupiterTime> Timelist = new List<JupiterTime>();
+
+    public void AddTime(JupiterTime time)
+    {
+        Timelist.Add(time);
+    }
+
+    public void Check(JupiterTime time)
+    {
+        string combinedList = string.Join("-", Timelist);
+        int TimelistInt = (combinedList[combinedList.Length - 4]) - '0';
+
+        string combinedTimeList = string.Join("-", time);
+        int TimeInt = (combinedTimeList[combinedTimeList.Length - 4]) - '0';
+
+        if (TimelistInt == TimeInt) Console.WriteLine("No signals needed to be sent yet");
+        else
+        {
+            for (int i = TimelistInt + 2; i != TimeInt; i += 2)
+            {
+                if (i == 10) i = 0;
+                Console.WriteLine($"0{i}:00");
+            }
+        }
+    }
+
+}

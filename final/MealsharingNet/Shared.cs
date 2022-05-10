e3b0c9c14c30600a283220475750c5298e73f9e5
semih1239 <semihayyildiz23@hotmail.com>
2022-05-10 18:36:07 +0200

    connection string updated with ssl key

--- final/MealsharingNet/Shared.cs
+++ final/MealsharingNet/Shared.cs
@@ -1,6 +1,6 @@
 public class Shared
 {
   public static string ConnectionString = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MYSQLCONNSTR_dotnet_meal_sharing")) ?
- "Server=localhost;Database=final-semih;Uid=root;Pwd=compl3xPassWrd;Convert Zero Datetime=True"   :
+ "Server=localhost;Database=final-semih;Uid=root;Pwd=compl3xPassWrd;SslMode=Required;Convert Zero Datetime=True"   :
  Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb");
 }
